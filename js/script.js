0631f3ee6efbaa9db44e8658ab6c186c1f64eb98
Prince Kumar <ku.prince1203@gmail.com>
2022-03-28 21:42:25 +0530

    enter

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-let ans=""
+let ans="";
+let check="";
 
 function addNum(a){
     ans = ans.concat(a);
     document.getElementById("math").innerHTML = ans;
+    check=a;
 }
 
 function addOpt(b){
-    ans = ans.concat(b);
+    if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
+        ans = (ans.slice(0,-1)).concat(b);
+    }
+    else{
+        ans = ans.concat(b);
+        check=b;
+    }
     document.getElementById("math").innerHTML = ans;
 }
 
-function cle(){
-    ans=""
-    document.getElementById("math").innerHTML = ans;
+function del(){
+    ans=" ";
+    document.getElementById("math").innerHTML = ans ;
     document.getElementById("display").innerHTML = "";
 }
 
 function backspace(){
-    ans=ans.slice(0, -1);;
+    ans=ans.slice(0, -1);
     document.getElementById("math").innerHTML = ans;
 }
 
 function Execute(){
-    document.getElementById("math").innerHTML = ans.concat('='); 
-    document.getElementById("display").innerHTML = eval(ans);
+    if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
+        console.log("exceute");
+        window.alert("Please Enter Number First after Operator");
+    }
+    else{
+        document.getElementById("math").innerHTML = ans.concat('='); 
+        document.getElementById("display").innerHTML = eval(ans);
+    }
+    
 }
