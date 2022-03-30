ca4419f331942fe7f4f26d7ab47c56a9ae170eb6
Prince Kumar <ku.prince1203@gmail.com>
2022-03-30 08:36:44 +0530

    enter

--- js/script.js
+++ js/script.js
@@ -4,42 +4,45 @@ let check="";
 function addNum(a){
     ans = ans.concat(a);
     document.getElementById("math").innerHTML = ans;
+    document.getElementById("display").innerHTML = eval(ans);
     check=a;
+    temp="";
 }
 
 function addOpt(b){
-    if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
+    if(ans.length==0 && (b=="*" || b=="/" || b=="%") || ans=="+" || ans=="-"){
+        window.alert("Enter Number First");
+    }
+    else if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
         ans = (ans.slice(0,-1)).concat(b);
     }
     else{
         ans = ans.concat(b);
-        check=b;
     }
+    check=b;
     document.getElementById("math").innerHTML = ans;
 }
 
 function cle(){
     ans=" ";
-    document.getElementById("math").innerHTML = ans;
-    document.getElementById("display").innerHTML = "";
-    
+    document.getElementById("display").innerHTML = ans;
+    document.getElementById("math").innerHTML = "";
 }
 
 function backspace(){
     ans=ans.slice(0, -1);
     document.getElementById("math").innerHTML = ans;
-    document.getElementById("display").innerHTML = "";
-
+    temp=ans.charAt(ans.length - 1);
 }
 
 function Execute(){
     if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
-        console.log("exceute");
-        window.alert("Please Enter Number First after Operator");
+        window.alert("Enter Number First after Operator");
     }
     else{
-        document.getElementById("math").innerHTML = ans.concat('='); 
+        document.getElementById("math").innerHTML = ""; 
         document.getElementById("display").innerHTML = eval(ans);
+        ans=eval(ans).toString();
     }
     
 }
