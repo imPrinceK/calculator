ed7b79e83ba30dc9a1fa0190658529873161cfb8
Prince Kumar <ku.prince1203@gmail.com>
2022-06-30 11:07:07 +0530

    Enter

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
-let ans="";
-let check="";
+let ans = "";
+let check = "";
 
-function addNum(a){
+function addNum(a) {
     ans = ans.concat(a);
     document.getElementById("math").innerHTML = ans;
     document.getElementById("display").innerHTML = eval(ans);
-    check=a;
-    temp="";
+    check = a;
+    temp = "";
 }
 
-function addOpt(b){
-    if(ans.length==0 && (b=="*" || b=="/" || b=="%") || ans=="+" || ans=="-"){
+function addOpt(b) {
+    if (ans.length == 0 && (b == "*" || b == "/" || b == "%") || ans == "+" || ans == "-") {
         window.alert("Enter Number First");
     }
-    else if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
-        ans = (ans.slice(0,-1)).concat(b);
+    else if (check == "*" || check == "+" || check == "-" || check == "/" || check == "%") {
+        ans = (ans.slice(0, -1)).concat(b);
     }
-    else{
+    else {
         ans = ans.concat(b);
     }
-    check=b;
+    check = b;
     document.getElementById("math").innerHTML = ans;
 }
 
-function cle(){
-    ans=" ";
+function cle() {
+    ans = " ";
     document.getElementById("display").innerHTML = ans;
     document.getElementById("math").innerHTML = "";
 }
 
-function backspace(){
-    ans=ans.slice(0, -1);
+function backspace() {
+    ans = ans.slice(0, -1);
     document.getElementById("math").innerHTML = ans;
-    temp=ans.charAt(ans.length - 1);
+    temp = ans.charAt(ans.length - 1);
 }
 
-function Execute(){
-    if(check=="*" || check=="+" || check=="-" || check=="/" || check=="%"){
+function Execute() {
+    if (check == "*" || check == "+" || check == "-" || check == "/" || check == "%") {
         window.alert("Enter Number First after Operator");
     }
-    else{
-        document.getElementById("math").innerHTML = ""; 
+    else {
+        document.getElementById("math").innerHTML = "";
         document.getElementById("display").innerHTML = eval(ans);
-        ans=eval(ans).toString();
+        ans = eval(ans).toString();
     }
 }
